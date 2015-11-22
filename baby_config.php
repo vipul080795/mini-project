6817e30c4a3ce99bb78be84205cd59759e71c31d
vipul080795 <vipul.badoni08@gmail.com>
2015-11-22 14:29:56 +0530

    Update baby_config.php

--- baby_config.php
+++ baby_config.php
@@ -2,7 +2,7 @@
 $host="localhost";
 $user="root";
 $pwd="";
-$db="babynames";
+$db="babynames"; //database name
 @mysql_connect($host,$user,$pwd) or die(mysql_error());
 @mysql_select_db($db) or die(mysql_error()) ;
 //echo "run succes";
