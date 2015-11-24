1b6bfa8f83b6f2a124159d9d0c33867608d6e682
vipul080795 <vipul.badoni08@gmail.com>
2015-11-24 23:15:18 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,31 +1,221 @@
+<!doctype html>
 <html>
-<title>::Find popularity of Name::</title>
-<body style="text-align: center; background-color: blanchedalmond ; backface-visibility: 50%;">
-<br />
-<br />
-<br />
-<hr color=228B22 />
-<form method="Post">
-<fieldset >
-<legend>Welcome Guest User</legend>
-<input type="radio" name="choise" required="" value="byname" /> Popularity & Rank Of Your Name
-<br />
-<input type="radio" name="choise" required="" value="byyear" /> Popular Name By Year
-<br />
-<input type="submit" value="GO" name="Go" />
-</fieldset>
-</form>
-<hr color=red />
-</body>
-<?php
-if(isset($_POST['Go']))
-{
-    $choise=$_POST['choise'];
-    $page= $choise.".php";
-    echo "Page Location : $page";
-    header("location:./$page");
-    
-}
-?>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Babynames</title>
 
+
+<link rel="stylesheet" href="css/reset.css" type="text/css" />
+<link rel="stylesheet" href="css/styles.css" type="text/css" />
+<link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+
+
+<!--[if lt IE 9]>
+<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/slider.js"></script>
+<script type="text/javascript" src="js/superfish.js"></script>
+
+<script type="text/javascript" src="js/custom.js"></script>
+
+<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0" />
+
+<!--
+spark, a free CSS web template by ZyPOP (zypopwebtemplates.com/)
+
+Download: http://zypopwebtemplates.com/
+
+License: Creative Commons Attribution
+//-->
+</head>
+<body>
+<div id="container">
+
+    <header> 
+	<div class="width">
+
+    		<h1><a href="index.php">Name<strong>s</strong></a></h1>
+
+		<nav>
+	
+    			<ul class="sf-menu dropdown">
+
+			
+        			<li class="selected"><a href="index.php">Home</a></li>
+
+            			<li>
+
+					<a href="byyear.php">Name by year</a>
+            			
+					<ul>
+                				<li><a href="male_names.php">Boy Name</a></li>
+						<li><a href="female_names.php">Girls Name</a></li>
+                    				<li><a href="both_names.php">Both</a></li>
+                			</ul>
+
+            			</li>
+
+            
+				<li>
+
+					<a href="byname.php">Popularity</a>
+            				
+					<ul>
+                				<li><a href="#">Name</a></li>
+                   				<li><a href="#">Year</a></li>
+                   				<li><a href="#">Both</a></li>
+                			</ul>
+
+            			</li>
+            
+				<li><a href="about.html">About</a></li>
+       			</ul>
+
+			
+			<div class="clear"></div>
+    		</nav>
+       	</div>
+
+	<div class="clear"></div>
+
+       
+    </header>
+
+
+    <div id="intro">
+
+	<div class="width">
+      
+		<div class="intro-content">
+	
+                    <h2>Baby Names Exclusively </h2>
+                    <p>Thanks for visiting my website</p>
+                                  	
+			<p><a href="byyear.php" class="button button-slider">Name by year</a>
+							<a href="byname.php" class="button button-reversed button-slider">Popularity </a></p>
+                    
+
+            	</div>
+                
+            </div>
+            
+
+	</div>
+
+    <div id="body" class="width">
+
+
+
+		<section id="content" class="two-column with-right-sidebar">
+
+	    <article>
+				
+			
+			<h2>Introduction to miniproject</h2>
+			<div class="article-info">Posted on <time datetime="2015-11-15">14 May</time> by <a href="#" rel="author">vipul Bloggs</a></div>
+
+            <p>Welcome to my website,<a href="http://zypopwebtemplates.com/" title="Twitter">Twitter</a>. This page is completely <strong>free</strong> to use permitting a link remains back to  <a href="http://zypopwebtemplates.com/" title="ZyPOP">http://zypopwebtemplates.com/</a>.</p>
+
+<p> Hello guys</p>	
+
+		<p>The image in the header <a href="http://Facebook.com/">UnSplash</a></p>
+            
+            <p>This page has been tested in:</p>
+
+
+            <ul class="styledlist">
+                <li>iMac</li>
+                <li>Windows</li>
+ 		<li>Android</li>
+                <li>Iphone</li>
+            </ul>
+
+		<a href="#" class="button">Read more</a>
+		<a href="#" class="button button-reversed">Comments</a>
+
+
+		
+		</article>
+	
+		<article class="expanded">
+
+		</article>
+        </section>
+        
+        <aside class="sidebar big-sidebar right-sidebar">
+	
+	
+            <ul>	
+               <li>
+                    <h2>Page List</h2>
+                    <ul class="blocklist">
+                        <li><a class="selected" href="index.html">Home Page</a></li>
+                        <li><a href="byyear.php">Names By year</a>
+				
+				        <li><a href="byname.php">Popularity By Graph</a></li>
+				
+				
+			</li>
+                        <li><a href="#">Male and Female</a></li>
+                        
+                    </ul>
+                </li>
+                
+               
+
+		
+               
+            
+	
+   		
+		
+        </aside>
+    	<div class="clear"></div>
+    </div>
+    <footer>
+        <div class="footer-content width">
+            <ul>
+            	<li><h4>Home</h4></li>
+                <li><a href="#">HOLA</a></li>
+            </ul>
+            
+            <ul>
+            	<li><h4>Names</h4></li>
+                <li><a href="#">vipul</a></li>
+            </ul>
+
+ 	    <ul>
+                <li><h4>Popularity</h4></li>
+                <li><a href="#">hurr</a></li>
+            </ul>
+            
+            <ul class="endfooter">
+            	<li><h4>About</h4></li>
+                <li> mini project 5th semester <br /><br />
+
+<div class="social-icons">
+
+<a href="#"><i class="fa fa-facebook fa-2x"></i></a>
+
+<a href="#"><i class="fa fa-twitter fa-2x"></i></a>
+
+<a href="#"><i class="fa fa-youtube fa-2x"></i></a>
+
+<a href="#"><i class="fa fa-instagram fa-2x"></i></a>
+
+</div>
+
+</li>
+            </ul>
+            
+            <div class="clear"></div>
+        </div>
+        <div class="footer-bottom">
+            <p>&copy; MySite 2015. <a href="http://babynames.com/">Contact: +91-9627774445</a> designed by Vipul</p>
+         </div>
+    </footer>
+</div>
+</body>
 </html>
