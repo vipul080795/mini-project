619bea86a0e93852ee71a0fec19a003afd043830
vipul080795 <vipul.badoni08@gmail.com>
2015-11-27 17:29:36 +0530

    Update search.php

--- search.php
+++ search.php
@@ -5,3 +5,25 @@ $password="";
 $con=mysqli_connect($host,$user,$password,'babynames')or die("error connecting to the database");
  
  $term=$_GET["term"];
+
+ 
+ $qur="(SELECT name FROM female_1992 where name like '".$term."%') UNION  (SELECT name FROM male_2001 where name like '".$term."%')";
+$result=mysqli_query($con,$qur)or die("error querying database"); 
+$json=array();
+ 
+//echo($qur);
+$i=0;
+while($row=mysqli_fetch_array($result))
+{
+if($i<10)
+{
+$json[]=array('value'=> $row[0]
+     );
+$i=$i+1;
+}
+}
+ 
+ 
+ echo json_encode($json);
+ 
+?>
